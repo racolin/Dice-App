a54a24e41e274e58fb5d0e4259676cb6d5903cc5
tin <tin.pt@ntnc.vn>
2023-06-24 22:55:02 +0700

    Initial Commit

---
+++ Dice App/Dice_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_AppApp.swift
+//  Dice App
+//
+//  Created by Administrator on 24/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct Dice_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
